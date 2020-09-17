7e36060eb1041bfa65168a680e9b0cb874a42cd2
JordanStuckings <jordan.stucking@gmail.com>
2020-09-17 08:41:22 +0000

    Deploying to gh-pages from  @ b7596350b6f2c96be8d687b6dbb26852676e806e 🚀

---
+++ precache-manifest.c7e3aa8722f254421f52296c611ff34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a924035b33e5d75496184073960c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa8747819594ba4b4d3",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa8747819594ba4b4d3",
+    "url": "/static/js/main.a55a5deb.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
