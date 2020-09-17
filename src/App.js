133bd66269f6678bfc68905f82a4b8fc385c8552
Jordan <jordan.stucking@gmail.com>
2020-09-17 01:39:10 -0700

    updated h2 in main page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello Jordan</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>My First Deployment</h2>
     </div>
   );
 }
