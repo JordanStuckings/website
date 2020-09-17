f1e2cbc5479d65bfd22e9d0ebe8ea6984fce2454
Jordan <jordan.stucking@gmail.com>
2020-09-17 08:23:59 +0000

    deploy

---
+++ docs/precache-manifest.df809fa32850c9a5ffbcaea5cfacde63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff85e11ebdc2daa77318cf5f7c862da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ae280ebf3148864bf8",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ae280ebf3148864bf8",
+    "url": "/static/js/main.9fe0bcbd.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
